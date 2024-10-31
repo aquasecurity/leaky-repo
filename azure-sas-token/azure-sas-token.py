009fc6a9e582800447c1dd63be9a4d1640e8ae72
JimAWiredu <157686773+JimAWiredu@users.noreply.github.com>
2024-10-31 07:54:55 +0000

    Create azure-sas-token.py
    azure-sas-token

---
+++ azure-sas-token/azure-sas-token.py
@@ -0,0 +1,3 @@
+def main():
+    azure_sas_token = "sv=2012-02-12&st=2009-02-09&se=2009-02-10&sr=c&sp=r&si=YWJjZGVmZw%3d%3d&sig=dD80ihBh5jfNpymO5Hg1IdiJIEvHcJpCMiCMnN%2fRnbI%3d"
+    return azure_sas_token
