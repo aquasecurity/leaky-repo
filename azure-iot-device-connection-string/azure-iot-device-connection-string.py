a1d59d86665bd17461a9d3ef7cbc910e0631b67b
JimAWiredu <157686773+JimAWiredu@users.noreply.github.com>
2024-11-06 12:07:30 +0000

    Create azure-iot-device-connection-string.py
    azure_iot_device_connection_string

---
+++ azure-iot-device-connection-string/azure-iot-device-connection-string.py
@@ -0,0 +1,3 @@
+def main():
+    azure_iot_device_connection_string =  "HostName=jimmyko.azure-devices.net;DeviceId=jimhub;SharedAccessKey=XKcA97AVdy9pxteYfckn+oi4Lot/7mReMlKhQ0tKMjo="
+    return azure_iot_device_connection_string
