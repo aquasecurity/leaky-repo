638f9100eec83257265e13730293dd694417033a
JimAWiredu <157686773+JimAWiredu@users.noreply.github.com>
2024-11-08 09:05:47 +0000

    Update azure-ad-client-secret.py (#109)
    fix azure client secret

--- azure-ad-client-secret/azure-ad-client-secret.py
+++ azure-ad-client-secret/azure-ad-client-secret.py
@@ -1,3 +1,3 @@
 def main():
-    client_secret = "bP88Q~rcBcYjzzOhg1Hnn76Wm3jGgakZiZ.8vMgR"
-    return client_secret
+    azure_client_secret = "bP88Q~rcBcYjzzOhg1Hnn76Wm3jGgakZiZ.8vMgR"
+    return azure_client_secret
