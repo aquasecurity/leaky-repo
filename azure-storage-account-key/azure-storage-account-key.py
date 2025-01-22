672665fd594047725672dea222d076c2393bd0a3
JimAWiredu <157686773+JimAWiredu@users.noreply.github.com>
2025-01-22 11:08:03 +0000

    Create azure-storage-account-key.py
    Create azure-storage-account-key

---
+++ azure-storage-account-key/azure-storage-account-key.py
@@ -0,0 +1,3 @@
+def main():
+    azure_storage_account_key = "azureAccountKey=g8FQca0QnXcrHvjU5PeMpbEv5sN2uwCPhoHbzcvdv7EF6LHLq/D96Rm4S9XRuKRcvff1xdShvAYElNH3NDZhnw=="
+    return azure_storage_account_key
