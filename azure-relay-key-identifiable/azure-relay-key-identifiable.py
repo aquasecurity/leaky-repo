66b1c7e0ade07684c0773c192fa5f9756ce7ff15
JimAWiredu <157686773+JimAWiredu@users.noreply.github.com>
2025-01-22 09:22:21 +0000

    Create azure-relay-key-identifiable.py
    azure_relay_key_identifiable

---
+++ azure-relay-key-identifiable/azure-relay-key-identifiable.py
@@ -0,0 +1,3 @@
+def main():
+    azure_relay_key_identifiable = "Endpoint=sb://mydemo.servicebus.windows.net/;SharedAccessKeyName=send;SharedAccessKey={base64 key};EntityPath=db","
+    return azure_relay_key_identifiable
