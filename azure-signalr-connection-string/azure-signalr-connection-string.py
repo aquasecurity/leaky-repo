7b36c5892cd237eadd62a3f9d038fcf45f624136
JimAWiredu <157686773+JimAWiredu@users.noreply.github.com>
2025-01-22 10:34:24 +0000

    Create azure-signalr-connection-string.py
    Create azure-signalr-connection-string

---
+++ azure-signalr-connection-string/azure-signalr-connection-string.py
@@ -0,0 +1,3 @@
+def main():
+    azure_signalr_connection_string = "Endpoint=https://<resource_name>.service.signalr.net;AccessKey=<access_key>;Version=1.0;"
+    return azure_signalr_connection_string
