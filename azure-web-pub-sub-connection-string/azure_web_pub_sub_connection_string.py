02e28137296184654af12f26cc6fa246d1d3aae9
JimAWiredu <157686773+JimAWiredu@users.noreply.github.com>
2025-01-22 11:13:35 +0000

    Create azure_web_pub_sub_connection_string.py (#124)
    Create azure_web_pub_sub_connection_string

---
+++ azure-web-pub-sub-connection-string/azure_web_pub_sub_connection_string.py
@@ -0,0 +1,3 @@
+def main():
+    azure_web_pub_sub_connection_string = "Endpoint=https://<resource-name>.webpubsub.azure.com;AccessKey=<access-key>;Version=1.0;"
+    return azure_web_pub_sub_connection_string
