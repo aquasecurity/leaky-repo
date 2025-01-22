1676a7ff1bc8b718a76142a8df2cde0a19462870
JimAWiredu <157686773+JimAWiredu@users.noreply.github.com>
2025-01-22 10:31:48 +0000

    Create azure_service_bus_identifiable.py (#120)
    azure_service_bus_identifiable

---
+++ azure_service_bus_identifiable/azure_service_bus_identifiable.py
@@ -0,0 +1,3 @@
+def main():
+    azure_service_bus_identifiable = "Endpoint=sb://myservicebusnamespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=abcd1234xyz="
+    return azure_service_bus_identifiable
