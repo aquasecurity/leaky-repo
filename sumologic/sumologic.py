6505a3625c1466981d751f797712ac1ed049ecf8
JimAWiredu <157686773+JimAWiredu@users.noreply.github.com>
2025-03-06 15:11:52 +0000

    Update sumologic.py
    fix sumo FP

--- sumologic/sumologic.py
+++ sumologic/sumologic.py
@@ -1,4 +1,3 @@
 def main():
-    sumologic_acces_id: 'suJ9efGLsgR2ZO'
-    sumologic_access_key = 'b7sdCwKq4ABG8NK9dyjzNltsjDKZdh1PLYVAYjgDYcQS65s61pU2FSMgJcGTjPDw'
-return sumo_access_id, sumo_access_key
+    sumologic_acces_id = 'suJ9efGLsgR2ZO' 
+    return sumo_access_id
