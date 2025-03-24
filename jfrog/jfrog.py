f8bc2e155b2984db3046b98320f1f72c176d9fc0
JimAWiredu <157686773+JimAWiredu@users.noreply.github.com>
2025-03-24 13:53:40 +0000

    Create jfrog.py
    adding jfrog keys

---
+++ jfrog/jfrog.py
@@ -0,0 +1,4 @@
+def main():
+    jfrog_api_key = "--set imagePullSecretJfrog.password=dzaf395f3s23hc691a66pll5xdsgf4sqar425gd4nfhncm4lmmblp67zct9vhyspnawoxsei7"
+    jfrog_api_token = "qb1cmrykdunt3t7rnqvizz98rnbkq5q78j58xyhw5ir7gw4i9erbdb6bkxf89abb"
+    return jfrog_api_key,jfrog_api_token
