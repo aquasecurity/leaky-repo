3300799c4b2711ab1cd4bfe20423db587ac74b4a
JimAWiredu <157686773+JimAWiredu@users.noreply.github.com>
2025-04-01 08:26:13 +0000

    Update jekyllfinegrain.py
    change name

--- jekyll/jekyllfinegrain.py
+++ jekyll/jekyllfinegrain.py
@@ -1,3 +1,3 @@
 def main():
-    jekyll_github_token_fine_grain = "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"
-    return jekyll_github_token_fine_grain
+    jekyll_github_token = "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"
+    return jekyll_github_token
