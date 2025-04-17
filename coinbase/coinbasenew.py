78e67bd8f23fc3cf4e653bc51b62be86ca8ce887
JimAWiredu <157686773+JimAWiredu@users.noreply.github.com>
2025-04-17 10:53:41 +0000

    Create coinbasenew.py (#131)
    adding keys

---
+++ coinbase/coinbasenew.py
@@ -0,0 +1,5 @@
+def main():
+    coinbase_api_secret = "8196df05-303e-4fd2-b372-d17eb29e2ab3"
+    coinbase_api_token = "mti9od_wmzryp5ne-wq__ip5thaooltmkuet0gr23wu_o7tekzmz_n11r_d_-exd"
+    coinbase_client_api_key ="L98XPSGT5TdZ8ddJZhEphQMBkDsSjFAz"
+    return coinbase_api_secret, coinbase_api_token, coinbase_client_api_key
