064f81bcdb02684ff6209301ebb041701635b827
JimAWiredu <157686773+JimAWiredu@users.noreply.github.com>
2025-04-17 12:13:30 +0000

    Update coinbasenew.py
    coinbase_secret

--- coinbase/coinbasenew.py
+++ coinbase/coinbasenew.py
@@ -2,4 +2,5 @@ def main():
     coinbase_api_secret = "8196df05-303e-4fd2-b372-d17eb29e2ab3"
     coinbase_api_token = "mti9od_wmzryp5ne-wq__ip5thaooltmkuet0gr23wu_o7tekzmz_n11r_d_-exd"
     coinbase_client_api_key ="L98XPSGT5TdZ8ddJZhEphQMBkDsSjFAz"
-    return coinbase_api_secret, coinbase_api_token, coinbase_client_api_key
+    coinbase_secret="pkqkd6h0SA+6g+eIo8xoJCPCMn3YVDxWDA4nVQMq2L+WyYhIx4J9xJnuTLC2eWeWstlEdEZvjE+zQXJr2Kj1pA=="
+    return coinbase_api_secret, coinbase_api_token, coinbase_client_api_key,coinbase_secret
