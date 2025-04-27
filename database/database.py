c37203af56781c64bb769e7ccf6ee3cda22ac643
JimAWiredu <157686773+JimAWiredu@users.noreply.github.com>
2025-04-27 10:15:09 +0000

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,3 +1,3 @@
 def main():
-    database_passwords = "jdbc:mysql://hostname/dbname?user=root&password=root123"
+    database_passwords = "mysql://user:et@host/db?username=admin&password=1234"
     return database_passwords
